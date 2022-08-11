583bd5c33188c1af9fd261b50c3a22a5eeec7169
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-08-11 18:34:17 -0500

    Added y-axis padding

--- master.js
+++ master.js
@@ -5,6 +5,8 @@ let WIDTH,
 const BG_COLOR = "black",
     LINE_COLOR = "white",
     RES = 200,
+    VEL = 0.5, // curve's speed in steps added per frame
+    PADDING_Y = 20,
     canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
 
@@ -41,17 +43,23 @@ function ani() {
     let canvasX = 0;
     let translatedX = (((c % RES) * step) / WIDTH) * Math.PI * 2;
     let y = Math.sin(translatedX);
-    ctx.moveTo(0, scale(y, -1, 1, HEIGHT, 0));
-    ctx.lineTo(scale(canvasX, 0, Math.PI * 2, 0, WIDTH), scale(y, -1, 1, HEIGHT, 0));
+    ctx.moveTo(0, scale(y, -1, 1, HEIGHT - PADDING_Y, PADDING_Y));
+    ctx.lineTo(
+        scale(canvasX, 0, Math.PI * 2, 0, WIDTH),
+        scale(y, -1, 1, HEIGHT - PADDING_Y, PADDING_Y)
+    );
     for (let i = 1; i < RES; i++) {
         canvasX = ((i * step) / WIDTH) * Math.PI * 2;
         translatedX = ((((i + c) % RES) * step) / WIDTH) * Math.PI * 2;
         y = Math.sin(translatedX);
-        ctx.lineTo(scale(canvasX, 0, Math.PI * 2, 0, WIDTH), scale(y, -1, 1, HEIGHT, 0));
+        ctx.lineTo(
+            scale(canvasX, 0, Math.PI * 2, 0, WIDTH),
+            scale(y, -1, 1, HEIGHT - PADDING_Y, PADDING_Y)
+        );
     }
     ctx.stroke();
     ctx.closePath();
-    c++;
+    c += VEL;
     requestAnimationFrame(ani);
 }
 
